fbd02746c71e7df34299a126e38e8a07f6ca5a84
Rodrigo Barros <brodrigo87@gmail.com>
2022-04-25 15:33:16 -0300

    load and resize images

--- tictactoe.py
+++ tictactoe.py
@@ -16,14 +16,26 @@ white = (255, 255, 255)
 line_color = (10, 10,10)
 
 # 3x3 Board
-
 TTT = [ [None]*3, [None] *3, [None] *3]
 
 
 # Initializing pygame window
-
 pg.init()
+
 fps = 30
 CLOCK = pg.time.Clock()
 screen = pg.display.set_mode((width, height+100), 0, 32)
+
 pg.display.set_caption("Tic Tac Toe")
+
+
+# loading the images
+
+opening = pg.image.load('tic tac opening.png')
+x_img = pg.image.load('X.png')
+o_img = pg.image.load('O.png')
+
+#resizing images
+x_img = pg.transform.scale(x_img, (80,80))
+o_img = pg.transform.scale(o_img, (80,80))
+opening = pg.transform.scale(opening, (width, height+100))
