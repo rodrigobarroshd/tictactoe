8bc336219863fa84b9b2ceab248520a18136a742
Rodrigo Barros <brodrigo87@gmail.com>
2022-04-25 15:46:44 -0300

    game opening

--- tictactoe.py
+++ tictactoe.py
@@ -39,3 +39,36 @@ o_img = pg.image.load('O.png')
 x_img = pg.transform.scale(x_img, (80,80))
 o_img = pg.transform.scale(o_img, (80,80))
 opening = pg.transform.scale(opening, (width, height+100))
+
+
+
+def game_opening():
+    screen.blit (opening, (0,0))
+    pg.display.update()
+    time.sleep(1)
+    screen.fill(white)
+    
+    # Drawing vertical lines
+    pg.draw.line(screen, line_color, (width/3,0), (width/3, height), 7)
+    pg.draw.line(screen,line_color,(width/3*2,0),(width/3*2, height),7)
+    
+    # Drawing horizontal lines
+    pg.draw.line(screen,line_color,(0,height/3),(width, height/3),7)
+    pg.draw.line(screen,line_color,(0,height/3*2),(width, height/3*2),7)
+    draw_status()
+    
+def draw_status():
+    global draw
+    if winner is None:
+        message = XO.upper() + "'s Turn"
+    else:
+        message = winner.upper() + " won!"
+    if draw:
+        message = 'Game Draw!'
+    font = pg.font.Font(None, 30)
+    text = font.render(message, 1, (255, 255, 255))
+    # copy the rendered message onto the board
+    screen.fill ((0, 0, 0), (0, 400, 500, 100))
+    text_rect = text.get_rect(center=(width/2, 500-50))
+    screen.blit(text, text_rect)
+    pg.display.update()
